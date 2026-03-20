4a1c8c3028cb8bbc14c9d1bd0b6e10bac2b9ac9e
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-20 06:00:19 +0100

    Dodany zalążek projektu mikroserwisu main_page_ms.

---
+++ main_page_ms/src/main/java/matinf/czasopismo/social/main_page_ms/MainPageMsApplication.java
@@ -0,0 +1,13 @@
+package matinf.czasopismo.social.main_page_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPageMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainPageMsApplication.class, args);
+	}
+
+}
---
+++ main_page_ms/src/test/java/matinf/czasopismo/social/main_page_ms/MainPageMsApplicationTests.java
@@ -0,0 +1,13 @@
+package matinf.czasopismo.social.main_page_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainPageMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
