8a02230356333ba0a3562bf5ab5c47ea3ed11414
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-20 11:34:30 +0100

    Dodane generowanie bazy danych social_pg_users w skrypcie sql.

--- docker-compose/initdb/init.sql
+++ docker-compose/initdb/init.sql
@@ -5,3 +5,34 @@ CREATE DATABASE social_pg_test
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
     TEMPLATE = template0;
+
+\c social_pg_test;
+
+CREATE DATABASE social_pg_users
+    WITH 
+    OWNER = pguser
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TEMPLATE = template0;
+
+\c social_pg_users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE(user_name)
+);
+
+CREATE TABLE IF NOT EXISTS user_page_attributes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    attribute_name TEXT NOT NULL,
+    attribute_value TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE(user_id, attribute_name)
+);
+
