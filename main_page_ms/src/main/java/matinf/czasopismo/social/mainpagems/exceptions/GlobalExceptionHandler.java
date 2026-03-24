29615927536d1d84a26361314b8d38b743a5b376
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-24 15:23:37 +0100

    Poprawienie logowania błędów w mainpage_ms.

--- main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/exceptions/GlobalExceptionHandler.java
+++ main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package matinf.czasopismo.social.mainpagems.exceptions;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import matinf.czasopismo.social.mainpagems.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,13 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
+        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.error(ex.toString());
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 ex.getMessage(),
@@ -24,6 +28,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
+        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.error(ex.toString());
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Unexpected error",
@@ -34,6 +40,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleInvalidParams(ConstraintViolationException ex) {
+        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.error(ex.toString());
         String message = ex.getConstraintViolations().stream()
                 .map(v -> v.getPropertyPath() + ": " + v.getMessage())
                 .findFirst()
