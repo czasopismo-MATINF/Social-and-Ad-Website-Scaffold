670a3aed8f8887c343b4a1db9f5ff96afe761e72
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-25 08:41:56 +0100

    Dodany szkielet projektu frontend_react_mui w osobnej gałęzi git.

---
+++ frontend_react_mui/src/App.jsx
@@ -0,0 +1,169 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import { useEffect } from "react";
+import keycloak from "./keycloak";
+
+function testApi(keycloak) {
+
+    console.log("TESTING API");
+    console.log(keycloak.tokenParsed);
+
+    fetch("http://localhost:3020/users/mikolaj", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + keycloak.token,
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+}
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+    keycloak.init({ onLoad: "check-sso" })
+      .then(() => setInitialized(true))
+      .catch(err => console.error("Keycloak init error:", err));
+  }, []);
+
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+
+      <div>
+      {!keycloak.authenticated ? (
+        <>
+          <p>Nie jesteś zalogowany</p>
+          <button onClick={() => keycloak.login()}>Zaloguj</button>
+        </>
+      ) : (
+        <>
+          <p>Witaj, {keycloak.tokenParsed?.preferred_username}</p>
+          {
+            testApi(keycloak)
+          }
+          <button onClick={() => keycloak.logout()}>Wyloguj</button>
+        </>
+      )}
+    </div>
+    
+    </>
+  )
+}
+
+export default App
