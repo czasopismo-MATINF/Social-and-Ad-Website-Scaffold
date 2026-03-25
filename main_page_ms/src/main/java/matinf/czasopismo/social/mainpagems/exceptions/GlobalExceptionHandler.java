c767d4b980e7ef96041faca3e749e9895d096d30
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-25 06:58:17 +0100

    Poprawka obsługi wyjątków w serwisie mainpage_ms.

--- main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/exceptions/GlobalExceptionHandler.java
+++ main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import matinf.czasopismo.social.mainpagems.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+
 import java.time.Instant;
 import java.time.ZoneOffset;
 
@@ -16,11 +19,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
-        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.info("Jestem w metodzie handleUserNotFound.", new Object() {}.getClass().getEnclosingMethod().getName());
         log.error(ex.toString());
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
-                ex.getMessage(),
+                "User not found",
                 Instant.now().atOffset(ZoneOffset.UTC)
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
@@ -28,7 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric(Exception ex) {
-        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.info("Jestem w metodzie: {}", new Object() {}.getClass().getEnclosingMethod().getName());
         log.error(ex.toString());
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -40,7 +43,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleInvalidParams(ConstraintViolationException ex) {
-        log.info("Jestem w metodzie: {}", new Object(){}.getClass().getEnclosingMethod().getName());
+        log.info("Jestem w metodzie: {}", new Object() {}.getClass().getEnclosingMethod().getName());
         log.error(ex.toString());
         String message = ex.getConstraintViolations().stream()
                 .map(v -> v.getPropertyPath() + ": " + v.getMessage())
@@ -54,5 +57,21 @@ public class GlobalExceptionHandler {
                         Instant.now().atOffset(ZoneOffset.UTC)
                 )
         );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleBodyError(HttpMessageNotReadableException ex) {
+        log.info("Jestem w metodzie: {}", new Object() {}.getClass().getEnclosingMethod().getName());
+        log.error(ex.toString());
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(400, "Invalid request body", Instant.now().atOffset(ZoneOffset.UTC)));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleBodyError(HttpMediaTypeNotSupportedException ex) {
+        log.info("Jestem w metodzie: {}", new Object() {}.getClass().getEnclosingMethod().getName());
+        log.error(ex.toString());
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(400, "Media type not supported", Instant.now().atOffset(ZoneOffset.UTC)));
     }
 }
--- main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/mappers/AttributeMapper.java
+++ main_page_ms/src/main/java/matinf/czasopismo/social/mainpagems/mappers/AttributeMapper.java
@@ -3,6 +3,8 @@ package matinf.czasopismo.social.mainpagems.mappers;
 import matinf.czasopismo.social.mainpagems.data.User;
 import matinf.czasopismo.social.mainpagems.data.UserPageAttribute;
 
+import java.time.LocalDateTime;
+
 public class AttributeMapper {
 
     public static void updateModelAttribute(UserPageAttribute modelAttribute, matinf.czasopismo.social.mainpagems.model.UserPageAttribute pageAttribute) {
@@ -13,12 +15,13 @@ public class AttributeMapper {
     }
 
     public static UserPageAttribute createModelAttribute(matinf.czasopismo.social.mainpagems.model.UserPageAttribute pageAttribute) {
-        return UserPageAttribute.builder().attributeName(pageAttribute.getAttributeName())
+        return UserPageAttribute.builder().attributeName(pageAttribute.getAttributeName()).createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .attributeValue(pageAttribute.getAttributeValue()).build();
     }
 
     public static UserPageAttribute createEmptyModelAttribute(String attributeName) {
-        return UserPageAttribute.builder().attributeName(attributeName).attributeValue("").build();
+        return UserPageAttribute.builder().createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).attributeName(attributeName).attributeValue("").build();
     }
 
 }
