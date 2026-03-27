1bda10a84312d7eb9d651f09e54c6da68cccb66e
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-27 19:32:16 +0100

    Gotowe pobieranie danych zalogowanego użytkownika na podstronę info.

--- frontend_react_mui/src/App.jsx
+++ frontend_react_mui/src/App.jsx
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 import keycloak from "./keycloak.js";
 
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from '../store/slice.js'
+import { increment, decrement, keycloakLoggedIn, keycloakLoggedOut, userInfoCollected } from '../store/slice.js'
 
 import { Link as MuiLink } from '@mui/material'
 import { Link } from 'react-router-dom'
@@ -18,20 +18,22 @@ import { Button } from '@mui/material'
 import Blog from '../blog/Blog.jsx'
 import UI from './UI.jsx'
 
-function testApi(keycloak) {
-
-    console.log("TESTING API");
-    console.log(keycloak.tokenParsed);
-
-    fetch("http://localhost:3020/users/mikolaj", {
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer " + keycloak.token,
-      "Content-Type": "application/json"
+function getUserInfo(keycloak, dispatch) {
+    console.log("GETTING USER INFO");
+    if(!keycloak.authenticated) {
+      console.log("User not authenticated, skipping user info fetch");
+      return;
     }
-  })
-    .then(res => res.json())
-    .then(data => console.log(data));
+    fetch(`http://localhost:3020/users/${keycloak.tokenParsed?.preferred_username}`, {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + keycloak.token,
+        "Content-Type": "application/json"
+      }
+    }).then(res => res.json())
+    .then(data => {
+      dispatch(userInfoCollected(data));
+    });
 
 }
 
@@ -41,9 +43,26 @@ function Main() {
   </UI>
 }
 
+function UserInfoComponent({ userInfo }) {
+  
+  if (!userInfo) {
+    return <p>Brak danych użytkownika.</p>;
+  }
+
+  return (
+    <>
+      {userInfo.attributes.map((e) => (
+        <p key={e.attributeName}>{e.attributeName}: {String(e.attributeValue)}</p>
+      ))}
+    </>
+  );
+
+}
+
 function Info() {
+  const userInfo = useSelector(state => state.example.userInfo);
   return <UI>
-    Info
+     <UserInfoComponent userInfo={userInfo}  />
   </UI>
 }
 
@@ -52,7 +71,6 @@ function Edit() {
     Edit
   </UI>
 }
-
 
 function A() {
   // Odczytaj wartość licznika ze stanu
@@ -104,11 +122,18 @@ function B() {
 
 function App() {
 
-  const [initialized, setInitialized] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-  keycloak.init({ onLoad: "check-sso" })
-    .then(() => setInitialized(true))
+    keycloak.init({ onLoad: "check-sso" })
+    .then(() => {
+      if (keycloak.authenticated) {
+        dispatch(keycloakLoggedIn());
+        getUserInfo(keycloak, dispatch);
+      } else {
+        dispatch(keycloakLoggedOut());
+      }
+    })
     .catch(err => console.error("Keycloak init error:", err));
   }, []);
 
@@ -129,122 +154,6 @@ function App() {
 
       </Routes>
     </BrowserRouter>
-  )
-}
-
-function InitApp() {
-
-  const [count, setCount] = useState(0)
-
-return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-
-    </>
   )
 }
 
