8025922ffcfd8cdf02aa9e4f8e08d0879df19f32
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-30 20:04:05 +0200

    Dodanie bazy danych dla mikroserwisu ogłoszeń z przykładowymi kategoriami.

--- docker-compose/initdb/init.sql
+++ docker-compose/initdb/init.sql
@@ -53,3 +53,39 @@ FROM new_user,
 
 COMMIT;
 
+CREATE DATABASE social_pg_ads
+    WITH 
+    OWNER = pguser
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TEMPLATE = template0;
+
+\c social_pg_ads;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    description TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS ads (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    category_id UUID NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+BEGIN;
+
+INSERT INTO categories (description) VALUES ('Motoryzacja');
+INSERT INTO categories (description) VALUES ('Nieruchomości');
+INSERT INTO categories (description) VALUES ('Elektronika');
+INSERT INTO categories (description) VALUES ('Moda');
+INSERT INTO categories (description) VALUES ('Dom i ogród');
+
+COMMIT;
