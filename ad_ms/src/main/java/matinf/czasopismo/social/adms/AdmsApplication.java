e8f2f4212a1e5e0f4c13981878c183b050bce673
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-03-30 17:12:11 +0200

    Dodanie szkieletu projektu mikroserwisu ad_ms.

---
+++ ad_ms/src/main/java/matinf/czasopismo/social/adms/AdmsApplication.java
@@ -0,0 +1,13 @@
+package matinf.czasopismo.social.adms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmsApplication.class, args);
+	}
+
+}
---
+++ ad_ms/src/test/java/matinf/czasopismo/social/adms/AdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package matinf.czasopismo.social.adms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
