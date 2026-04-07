6e69d24973fae066499b27b826b0be31281eccb8
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-04-07 13:26:26 +0200

    Drobne poprawki w strukturze komponentów.

--- frontend_react_mui/src/wrappers/EditAdComponent.jsx
+++ frontend_react_mui/src/wrappers/EditAdComponent.jsx
@@ -2,10 +2,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import AppTheme from '../../shared-theme/AppTheme.jsx';
 import AppAppBar from '../components/AppAppBar.jsx';
-import MainContent from '../components/MainContent.jsx';
-import Latest from '../components/Latest.jsx';
 import Footer from '../components/Footer.jsx';
 import EditAd from '../components/EditAd.jsx';
+import PageContent from '../components/PageContent.jsx';
 
 export default function EditAdsComponent(props) {
   return (
@@ -17,7 +16,7 @@ export default function EditAdsComponent(props) {
         component="main"
         sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4, pt: 10 }}
       >
-        <EditAd />
+        <PageContent><EditAd /></PageContent>
       </Container>
       <Footer />
     </AppTheme>
