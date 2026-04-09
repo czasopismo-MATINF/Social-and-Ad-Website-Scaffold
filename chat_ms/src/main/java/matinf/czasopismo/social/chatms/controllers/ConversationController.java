7a2b360d31e0f396c49f3f66676ead4be76667e5
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-04-09 07:44:55 +0200

    Proponowana wersja api.yml dla mikroserwisu chat_ms, ale bez paginacji - bardzo prosta wersja.

--- chat_ms/src/main/java/matinf/czasopismo/social/chatms/controllers/ConversationController.java
+++ chat_ms/src/main/java/matinf/czasopismo/social/chatms/controllers/ConversationController.java
@@ -1,6 +1,8 @@
 package matinf.czasopismo.social.chatms.controllers;
 
 import matinf.czasopismo.social.chatms.api.ConversationsApi;
+import matinf.czasopismo.social.chatms.model.ConversationPage;
+import matinf.czasopismo.social.chatms.model.ConversationsListPage;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -9,8 +11,13 @@ import java.util.UUID;
 public class ConversationController implements matinf.czasopismo.social.chatms.api.ConversationsApi {
 
     @Override
-    public ResponseEntity<Void> conversationsPost(List<UUID> participants) {
-        return ConversationsApi.super.conversationsPost(participants);
+    public ResponseEntity<ConversationsListPage> conversationsGet(List<UUID> participants) {
+        return ConversationsApi.super.conversationsGet(participants);
+    }
+
+    @Override
+    public ResponseEntity<ConversationPage> conversationsIdGet(UUID id, Boolean withMessages) {
+        return ConversationsApi.super.conversationsIdGet(id, withMessages);
     }
 
 }
