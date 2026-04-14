0ddf488c1bb96f4840c22fa59b49b5cac42061f3
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-04-14 10:58:41 +0200

    Dodanie prostej strony głównej z listą kategorii.

--- frontend_react_mui_2/src/App2.jsx
+++ frontend_react_mui_2/src/App2.jsx
@@ -7,6 +7,7 @@ import AdsListPage from "./pages/AdsListPage";
 import UserInfoPage from "./pages/UserInfoPage";
 import UserInfoEditRawPage from "./pages/UserInfoEditRawPage.jsx";
 import UserInfoEditPage from "./pages/UserInfoEditPage.jsx";
+import MainPage from "./pages/MainPage.jsx";
 
 import { useSelector, useDispatch } from 'react-redux'
 
@@ -102,7 +103,7 @@ const App = () => {
     <BrowserRouter>
       <AppLayout>
         <Routes>
-          <Route path="/" element={<div>Witaj na stronie głównej</div>} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/userads" element={<UserAdsListPage />} />
           <Route path="/ads" element={<AdsListPage />} />
