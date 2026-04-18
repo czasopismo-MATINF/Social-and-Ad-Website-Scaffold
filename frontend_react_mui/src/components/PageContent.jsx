d8cf8a943f30e705e625fddf75ac8ad641244c4f
Czasopismo MATINF <czasopismo.matinf@gmail.com>
2026-04-18 22:13:14 +0200

    Usunięcie starego projektu frontendu frontend_react_mui, który już nie będzie rozwijany.

--- frontend_react_mui/src/components/PageContent.jsx
+++
@@ -1,4 +0,0 @@
-
-export default function PageContent({children}) {
-    return <>{children}</>
-}
--- frontend_react_mui/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import reducer from './slice.js'
-
-export const store = configureStore({
-  reducer: {
-    example: reducer
-  }
-})
